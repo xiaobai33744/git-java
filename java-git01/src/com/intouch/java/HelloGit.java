9421fc2c077552047c95ba1258747590d68888c0
root <3374423145@qq.com>
2020-12-03 10:04:20 +0800

    实施了第二次修改

--- java-git01/src/com/intouch/java/HelloGit.java
+++ java-git01/src/com/intouch/java/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
     public static void main(String[] args){
         System.out.println("Hello,Git");
         System.out.println("update 1");
+        System.out.println("update 2");
     }
 }
